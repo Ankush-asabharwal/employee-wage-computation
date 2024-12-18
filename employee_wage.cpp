7e39b66aead9cae00b636b8b31ceb5ae4967a7d1
Ankush-asabharwal <ankusha0209.be21@gmail.com>
2024-12-18 11:51:19 +0530

    Adding branch

--- employee_wage.cpp
+++ employee_wage.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main(){
     cout << "Welcome to Employee wage computation"<<endl;
-    return 0;
+
+    // srand(1);
+    // for(int i=0;i<3;i++){
+    //     cout << rand() << endl;
+    // }
+
+    // int randomNum = rand()%2;
+    // if(randomNum==1){
+    //     cout << "Employee is present" << endl;
+    // }
+    // else{
+    //     cout << "Employee is not present" << endl;
+    // }
 }
