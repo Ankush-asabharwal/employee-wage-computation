3b565bcc413ffd0ec41d938637d5260841c4db59
Ankush-asabharwal <ankusha0209.be21@gmail.com>
2024-12-23 13:54:32 +0530

    [Ankusha] add : UC2 task is complete and now adding to git

--- employee_wage.cpp
+++ employee_wage.cpp
@@ -2,19 +2,24 @@
 
 using namespace std;
 
+void wageCalc(int wagePerHr,int randN){
+    if(randN==0){
+        cout << "Total wage for full-time employee is : ";
+        cout << 8 * wagePerHr;
+    }
+}
+
 int main(){
+    srand(time(0));
     cout << "Welcome to Employee wage computation"<<endl;
-
-    // srand(1);
-    // for(int i=0;i<3;i++){
-    //     cout << rand() << endl;
-    // }
-
-    // int randomNum = rand()%2;
-    // if(randomNum==1){
-    //     cout << "Employee is present" << endl;
-    // }
-    // else{
-    //     cout << "Employee is not present" << endl;
-    // }
+    int wagePerHr =20;
+    int randomNum = rand()%2;
+    if(randomNum==1){
+        cout << "Employee is present" << endl;
+        int randN = rand()%2;
+        wageCalc(wagePerHr, randN);
+    }
+    else{
+        cout << "Employee is not present" << endl;
+    }
 }
