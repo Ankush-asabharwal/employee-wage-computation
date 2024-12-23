97cc4c71d88e8268959d03cebc153e0342ffcd9d
Ankush-asabharwal <ankusha0209.be21@gmail.com>
2024-12-23 14:13:12 +0530

    [Ankusha] add: UC4 is complete and now adding to git

--- employee_wage.cpp
+++ employee_wage.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 
 void wageCalc(int wagePerHr,int randN){
-    if(randN==0){
-        cout << "Total wage for full-time employee is : ";
-        cout << 8 * wagePerHr;
-    }
-    else{
-        cout << "Total wage for part-time employee is : ";
-        cout << 4 * wagePerHr;
+    switch(randN){
+        case 0 :
+            cout << "Total wage for full-time employee is : ";
+            cout << 8 * wagePerHr;
+            break;
+        case 1 :
+            cout << "Total wage for part-time employee is : ";
+            cout << 4 * wagePerHr;
+            break;
     }
 }
 
@@ -18,12 +20,14 @@ int main(){
     cout << "Welcome to Employee wage computation"<<endl;
     int wagePerHr =20;
     int randomNum = rand()%2;
-    if(randomNum==1){
-        cout << "Employee is present" << endl;
-        int randN = rand()%2;
-        wageCalc(wagePerHr, randN);
-    }
-    else{
-        cout << "Employee is not present" << endl;
+    switch(randomNum){
+        case 0 :
+            cout << "Employee is not present" << endl;
+            break;
+        case 1 :
+            cout << "Employee is present" << endl;
+            int randN = rand()%2;
+            wageCalc(wagePerHr, randN);
+            break;
     }
 }
